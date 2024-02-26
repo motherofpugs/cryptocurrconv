01b28db696827d5c16062521b52a14b89215723b
motherofpugs <csampaidominika@gmail.com>
2024-02-26 20:42:22 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginpageComponent } from './components/loginpage/loginpage.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginpageComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
