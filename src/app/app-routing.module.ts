517bf1141c0ef1b5dfc5523d65e3b006db73cee1
motherofpugs <csampaidominika@gmail.com>
2024-02-27 19:35:18 +0100

    added some routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginpageComponent } from './components/loginpage/loginpage.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'loginpage/login', pathMatch: 'full' },
+  {
+    path: 'loginpage',
+    component: LoginpageComponent,
+    children: [
+      { path: 'signup', component: SignupComponent },
+      { path: 'login', component: LoginComponent },
+
+      { path: '**', redirectTo: '/loginpage/login' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
